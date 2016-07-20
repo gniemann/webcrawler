2f13bb283dee995901e23d8540cfb2f1e11af7b5
Ashton Herrington <herrinas@flip3.engr.oregonstate.edu>
2016-07-19 19:59:49 -0700

    Reorder file structure

---
+++ javascript/lowpass.js
@@ -0,0 +1,32 @@
+﻿// Simple lowpass filter for smoothing.
+// http://en.wikipedia.org/wiki/Low-pass_filter
+var Lowpass = function (alpha) {
+    this.firstrun = true;
+    this.y;
+    this.alpha = alpha;
+    
+    /// <summary>
+    /// Calculate alpha filter parameter from sample interval and cutoff frequency.
+    /// </summary>
+    /// <param name="dt">Sample interval (in seconds)</param>
+    /// <param name="fc">Cutoff frequency</param>
+    /// <returns>Alpha parameter.</returns>
+    this.calculateAlpha = function (dt, fc) {
+        return dt / ((1.0 / fc) + dt);
+    }
+    
+    /// <summary>
+    /// Apply filter to an input sample.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns>Current filtered output value.</returns>
+    this.update = function(x)
+    {
+        if (this.firstrun) {
+            this.y = x;
+            this.firstrun = false;
+        }
+        else this.y = this.y + this.alpha * (x - this.y);
+        return this.y;
+    }
+}
