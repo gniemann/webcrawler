0022b23b8c99ce2f5b9d7ebef7bcea356026af4c
Greg Niemann <greg.niemann@gmail.com>
2016-08-06 14:48:44 -0500

    Corrects error in favicon regular expression

--- remote_tests.py
+++ remote_tests.py
@@ -100,9 +100,9 @@ class TestGetPage(unittest.TestCase):
 
     def conduct_test(self, searcb_type):
         submit_data = {
-            'start_page': 'https://www.google.com/',
+            'start_page': 'http://www.mkdocs.org/',
             'search_type': searcb_type,
-            'depth': 11
+            'depth': 2
         }
         res = requests.post(BASE_URL + 'crawler', data=submit_data)
         self.assertEqual(200, res.status_code)
